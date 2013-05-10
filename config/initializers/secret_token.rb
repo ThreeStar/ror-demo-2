134d2d782654bd422665593b796b9fa5af32c065
Blues <blues@jpluscorp.com>
2013-05-10 14:28:28 +0800

    using rspec test framework.init commit for ror demo 2.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorDemo2::Application.config.secret_token = '866b7f156b584d28937f0cb5f6376cf69e3dafbfe6948de09007cc6436e46913f86b441de50948bf12c1f3c3852c90d0e88d1528d41771d715e990b3a819c1c9'
