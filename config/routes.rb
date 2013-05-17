1a871351562015307e22b6c1c8c6be511d5727fe
Blues <blues@jpluscorp.com>
2013-05-17 16:17:19 +0800

    finish sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 RorDemo2::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # get "users/new"
 
   # get "static_pages/home"
@@ -9,6 +10,10 @@ RorDemo2::Application.routes.draw do
   root to: 'static_pages#home'
   
   match '/signup', to: 'users#new'
+  
+  match '/signin', to: 'sessions#new'
+  
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # get "static_pages/help"
   match '/help', to: 'static_pages#help'
