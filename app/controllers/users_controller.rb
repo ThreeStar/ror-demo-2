77f963eaecbfdcdda02411d4c38097e477d2681f
Blues <blues@jpluscorp.com>
2013-06-27 16:32:16 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -48,10 +49,10 @@ class UsersController < ApplicationController
   
   private
   
-    def signed_in_user
-      store_location
-      redirect_to signin_path, notice: "Please sign in." unless signed_in?
-    end
+    # def signed_in_user
+    #   store_location
+    #   redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    # end
     
     def correct_user
       @user = User.find(params[:id])
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,6 +42,22 @@ describe "Static pages" do
     #   # visit root_path
     #   page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
     # end
+    
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+      
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
   
   describe "Help page" do
